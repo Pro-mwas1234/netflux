a2903325d2cf23f788400c84f4c30d49dbd9d358
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-01-13 12:35:49 +0300

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+// components/Header.tsx
+export default function Header() {
+  return (
+    <header className="bg-black p-6 border-b border-gray-800">
+      <h1 className="text-red-600 text-3xl font-bold">WATCHIFY</h1>
+    </header>
+  );
+}
