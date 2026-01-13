e50efd0a9aa68a116e9232bf1906d6ff3fe11769
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-01-13 16:05:24 +0300

    Update MovieCard.tsx

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,11 +3,10 @@ import Link from 'next/link';
 
 export default function MovieCard({ movie }: { movie: any }) {
   return (
-    <Link href={`/movie/${movie.id}`} className="block">
+    <Link href={`/movie/${movie.id}`} className="movie-card">
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
-        className="w-full rounded-lg"
       />
     </Link>
   );
