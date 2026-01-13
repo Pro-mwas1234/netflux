41add47481f7f5af5099f1711a2ae3461142c347
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-01-13 13:04:13 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
-// components/Header.tsx
 export default function Header() {
   return (
     <header className="bg-black p-6 border-b border-gray-800">
-      <h1 className="text-red-600 text-3xl font-bold">WATCHIFY</h1>
+      <h1 className="text-red-600 text-3xl font-bold">NETFLUX</h1>
     </header>
   );
 }
