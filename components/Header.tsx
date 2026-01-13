e2a73e3dbf42c159672e8d4a5810862b1d710d12
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-01-13 16:44:32 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
-// components/Header.tsx
 export default function Header() {
   return (
     <header className="topbar">
       <div className="logo">NETFLUX</div>
-      <img src="./user.png" alt="User" className="user-icon" />
+      <div>👤</div>
     </header>
   );
 }
