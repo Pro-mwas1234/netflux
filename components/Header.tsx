5ceaa2f82b79617f7666070b9b681ce1aab93b74
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-01-13 14:44:12 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,14 @@
 // components/Header.tsx
 export default function Header() {
   return (
-    <header className="bg-black p-6 border-b border-gray-800">
-      <h1 className="text-red-600 text-3xl font-bold">NETFLUX</h1>
-    </header>
+    <div className="flex items-center justify-between">
+      <h1 className="text-red-600 text-4xl font-bold">NETFLUX</h1>
+      <div className="flex space-x-4">
+        <span>Home</span>
+        <span className="text-gray-400">Series</span>
+        <span className="text-gray-400">Films</span>
+        <span className="text-gray-400">New & Popular</span>
+      </div>
+    </div>
   );
 }
